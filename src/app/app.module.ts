0266a996bcc86a829d2233f17c65e127135bc4a6
Sarath <sarath.hacks@gmail.com>
2018-11-08 21:51:44 +0530

    :boom: About page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    HomeComponent,
+    NavigationComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
